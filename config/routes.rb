4f6e1c83b4fde20c48b296ae821f08fd4d6f8eb3
efren <efren@favoritemedium.com>
2013-03-19 11:52:41 +0800

    Added new index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Sample3columns::Application.routes.draw do
+  # get "pages/home"
+
+  root :to => "pages#home"
   # The priority is based upon order of creation:
-  # first created -> highest priority.
+  # first created [-> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
